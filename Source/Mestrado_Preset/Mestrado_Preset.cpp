002a09a70aac329ec355b8c0ed353caf0206e81f
Gustavo Correa de Almeida <GCORREAA@edu.unisinos.br>
2023-12-18 14:29:01 -0300

    Start project.

---
+++ Source/Mestrado_Preset/Mestrado_Preset.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mestrado_Preset.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Mestrado_Preset, "Mestrado_Preset" );
